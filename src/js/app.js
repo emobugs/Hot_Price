634880c43c7d7939b03f77083794ad7896540b27
Emo <emotupev11@gmail.com>
2023-01-27 23:27:59 +0200

    feat: add ui functionality

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let prices = document.querySelectorAll(".price");
+
+  prices.forEach(p => {
+    if(p.classList.contains("hot")){
+      let text = p.innerHTML;
+      // check for parentElement if cant pass
+      p.innerHTML = `${text}&#128293;`;
+    }
+  })
 });
